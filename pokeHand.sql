a573e08ce50eae3ec1521a3518e20b20b28bec93
Douglas André Cigognini <50336514+DouglasAndre25@users.noreply.github.com>
2021-07-02 17:24:52 -0300

    feat: add rules, default and calculate fields
    + on delete constraint rules

--- pokeHand.sql
+++ pokeHand.sql
@@ -5,12 +5,16 @@ GO
 USE PokeHand
 GO
 
+CREATE RULE negative_range AS @range >= 0
+GO
+
 -- Tabela Treiandor
 CREATE TABLE trainer (
     id INT IDENTITY(1,1),
     name NVARCHAR(50) NOT NULL,
     age INT NOT NULL,
     vocation NVARCHAR(50) NOT NULL,
+    nick_name AS name + '@' + vocation PERSISTED,
     CONSTRAINT pk_trainer PRIMARY KEY (id)
 );
 GO
@@ -38,28 +42,27 @@ GO
 CREATE TABLE attack (
     id INT IDENTITY(1,1),
     name NVARCHAR(50) NOT NULL,
-    damage REAL NOT NULL,
-    power_point INT NOT NULL,
-    accuracy REAL NOT NULL,
-    type_id INT NOT NULL,
+    damage REAL NOT NULL DEFAULT 20,
+    power_point INT NOT NULL DEFAULT 10,
+    accuracy REAL NOT NULL DEFAULT 100,
+    type_id INT,
     CONSTRAINT pk_attack PRIMARY KEY (id),
-    CONSTRAINT fk_attack_id8 FOREIGN KEY (type_id) REFERENCES type (id)
+    CONSTRAINT fk_attack_id8 FOREIGN KEY (type_id) REFERENCES type (id) ON DELETE SET NULL
 );
 GO
 
 -- Tabela treinador_pokemon
 CREATE TABLE trainer_pokemon (
     id INT IDENTITY(1,1),
-    life INT NOT NULL,
-    level INT NOT NULL,
+    life INT NOT NULL DEFAULT 0,
+    level INT NOT NULL DEFAULT 1,
     trainer_id INT NOT NULL,
     pokemon_id INT NOT NULL,
     CONSTRAINT pk_trainer_pokemon PRIMARY KEY (id),
-    CONSTRAINT fk_trainer_id FOREIGN KEY (trainer_id) REFERENCES trainer (id),
-    CONSTRAINT fk_pokemon_id1 FOREIGN KEY (pokemon_id) REFERENCES pokemon (id)
+    CONSTRAINT fk_trainer_id FOREIGN KEY (trainer_id) REFERENCES trainer (id) ON DELETE CASCADE,
+    CONSTRAINT fk_pokemon_id1 FOREIGN KEY (pokemon_id) REFERENCES pokemon (id) ON DELETE CASCADE
 );
 GO
-
 
 -- Tabela ataque_pokemon
 CREATE TABLE attack_pokemon (
@@ -67,7 +70,17 @@ CREATE TABLE attack_pokemon (
     pokemon_id INT NOT NULL,
     attack_id INT NOT NULL,
     CONSTRAINT pk_attack_pokemon PRIMARY KEY (id),
-    CONSTRAINT fk_pokemon_id4 FOREIGN KEY (pokemon_id) REFERENCES pokemon (id),
-    CONSTRAINT fk_attack_id1 FOREIGN KEY (attack_id) REFERENCES attack (id),
+    CONSTRAINT fk_pokemon_id4 FOREIGN KEY (pokemon_id) REFERENCES pokemon (id) ON DELETE CASCADE,
+    CONSTRAINT fk_attack_id1 FOREIGN KEY (attack_id) REFERENCES attack (id) ON DELETE CASCADE,
 );
+GO
+
+EXECUTE sp_bindrule negative_range, 'trainer.age'
+EXECUTE sp_bindrule negative_range, 'pokemon.weight'
+EXECUTE sp_bindrule negative_range, 'pokemon.height'
+EXECUTE sp_bindrule negative_range, 'attack.damage'
+EXECUTE sp_bindrule negative_range, 'attack.power_point'
+EXECUTE sp_bindrule negative_range, 'attack.accuracy'
+EXECUTE sp_bindrule negative_range, 'trainer_pokemon.life'
+EXECUTE sp_bindrule negative_range, 'trainer_pokemon.level'
 GO
